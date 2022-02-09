9a333216fb6918c2fb53beb449631bbf20a3c24c
Alina Guo <jguo54@salz2.idies.jhu.edu>
2022-02-09 18:58:36 -0500

    added accessory python script jellyfish.py

---
+++ src/jellyfish.py
@@ -0,0 +1,15 @@
+import sys
+import csv
+import math
+csv_file = sys.argv[1]
+count = math.inf
+threshold = 0
+with open(csv_file,'r') as histo:
+    csvreader = csv.reader(histo,delimiter=' ')
+    for row in csvreader:
+        if count >= int(row[-1]):
+            count = int(row[-1])
+            threshold = int(int(row[0])/2)
+        else: #found local min
+            sys.stdout.write(str(threshold))
+            sys.exit(0)
