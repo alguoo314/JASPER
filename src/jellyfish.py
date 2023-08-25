1cb78433abb9bc862e38c5668387b9760c17222a
Aleksey Zimin <alekseyz@salz2.idies.jhu.edu>
2023-08-25 09:13:25 -0400

    improved jellyfish.py -- sometimes math.inf is not available

--- src/jellyfish.py
+++ src/jellyfish.py
@@ -3,17 +3,20 @@ import sys
 import csv
 import math
 csv_file = sys.argv[1]
-count = 10000000
+count = -1
 threshold = 0
 with open(csv_file,'r') as histo:
     csvreader = csv.reader(histo,delimiter=' ')
     for row in csvreader:
-        if count >= int(row[-1]):
-            count = int(row[-1])
-            threshold = int(int(row[0])/2)
-        else: #found local min
-            if threshold<2:
-                sys.exit(0)
-            else:
-                sys.stdout.write(str(threshold))
-                sys.exit(0)
+        if count == -1:
+            count= int(row[-1])
+        else:
+            if count >= int(row[-1]):
+                count = int(row[-1])
+                threshold = int(int(row[0])/2)
+            else: #found local min
+                if threshold<2:
+                    sys.exit(1)
+                else:
+                    sys.stdout.write(str(threshold))
+                    sys.exit(0)
